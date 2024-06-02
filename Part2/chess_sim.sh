238e9165b6c06f8922942efb93a513d569869ae4
ilanitb16 <ilanit.berdichevski@gmail.com>
2024-06-02 17:08:19 +0300

    update code

--- Part2/chess_sim.sh
+++ Part2/chess_sim.sh
@@ -1,93 +1,92 @@
-#!/bin/bash
+msg1="Press 'd' to move forward, 'a' to move back, 'w' to go to the start, 's' to go to the end. 'q' to quit:"
+msg2=Exiting.
 
-# Function to display the chess board
-print_board() {
-    clear
-    echo "Move $1/${#moves_history[@]}"
-    for ((i=0; i<8; i++)); do
-        echo -n "$((8-i)) "
-        for ((j=0; j<8; j++)); do
-            echo -n "${board[i*8+j]} "
-        done
-        echo "$((8-i))"
+moves=("6 4 4 4")
+
+# Define Unicode characters for chess pieces
+WHITE_PAWN="♙"
+BLACK_PAWN="♟"
+WHITE_ROOK="♖"
+BLACK_ROOK="♜"
+WHITE_KNIGHT="♘"
+BLACK_KNIGHT="♞"
+WHITE_BISHOP="♗"
+BLACK_BISHOP="♝"
+WHITE_QUEEN="♕"
+BLACK_QUEEN="♛"
+WHITE_KING="♔"
+BLACK_KING="♚"
+
+# Initialize the chessboard
+board=(
+    $BLACK_ROOK $BLACK_KNIGHT $BLACK_BISHOP $BLACK_QUEEN $BLACK_KING $BLACK_BISHOP $BLACK_KNIGHT $BLACK_ROOK
+    $BLACK_PAWN $BLACK_PAWN $BLACK_PAWN $BLACK_PAWN $BLACK_PAWN $BLACK_PAWN $BLACK_PAWN $BLACK_PAWN
+    "." "." "." "." "." "." "." "."
+    "." "." "." "." "." "." "." "."
+    "." "." "." "." "." "." "." "."
+    "." "." "." "." "." "." "." "."
+    $WHITE_PAWN $WHITE_PAWN $WHITE_PAWN $WHITE_PAWN $WHITE_PAWN $WHITE_PAWN $WHITE_PAWN $WHITE_PAWN
+    $WHITE_ROOK $WHITE_KNIGHT $WHITE_BISHOP $WHITE_QUEEN $WHITE_KING $WHITE_BISHOP $WHITE_KNIGHT $WHITE_ROOK
+)
+
+# Function to move a piece
+move_piece() {
+    local start_row=$1
+    local start_col=$2
+    local end_row=$3
+    local end_col=$4
+
+    local start_index=$((start_row * 8 + start_col))
+    local end_index=$((end_row * 8 + end_col))
+
+    board[$end_index]=${board[$start_index]}
+    board[$start_index]=" "
+}
+
+function display_board(){
+  local i j
+
+  for ((i=0; i<8; i++)); do
+      for ((j=0; j<8; j++)); do
+          index=$((i * 8 + j))
+          printf "${board[$index]} "
+      done
+      echo
+  done
+}
+
+while true
+do
+  display_board
+  echo $msg1
+  read line
+  
+  case $line in
+    "q")
+    echo $msg2
+    exit 0
+    ;;
+
+    "d")
+    echo Move forward
+
+    for move in "${moves[@]}"; do
+      set -- $move
+      move_piece $1 $2 $3 $4
     done
-    echo "  a b c d e f g h"
-}
+    ;;
 
-# Function to initialize the chess board
-init_board() {
-    board=(r n b q k b n r \
-           p p p p p p p p \
-           . . . . . . . . \
-           . . . . . . . . \
-           . . . . . . . . \
-           . . . . . . . . \
-           P P P P P P P P \
-           R N B Q K B N R)
-}
+    "a")
+    echo Move back
+    ;;
 
-# Function to load the PGN file and parse moves using Python script
-load_pgn() {
-    if [ ! -f "$1" ]; then
-        echo "File does not exist: $1"
-        exit 1
-    fi
-    
-    echo "Metadata from PGN file:"
-    grep -E '^\[.*\]$' "$1"
-    
-    moves_pgn=$(grep -E '^[0-9]+\.' "$1" | tr '\n' ' ')
-    moves_uci=($(python3 parse_moves.py "$moves_pgn"))
-    moves_history=("${moves_uci[@]}")
-}
+    "w")
+    echo Go to the start
+    ;;
 
-# Function to move forward
-move_forward() {
-    if [ "$move_index" -lt "${#moves_history[@]}" ]; then
-        move=${moves_history[move_index]}
-        board[${move:2:1}${move:3:1}]=${board[${move:0:1}${move:1:1}]}
-        board[${move:0:1}${move:1:1}]="."
-        ((move_index++))
-    else
-        echo "No more moves available."
-    fi
-}
+    "s")
+    echo Go to the start
+    ;;
+  esac
 
-# Function to move backward
-move_backward() {
-    if [ "$move_index" -gt 0 ]; then
-        ((move_index--))
-        move=${moves_history[move_index]}
-        board[${move:0:1}${move:1:1}]=${board[${move:2:1}${move:3:1}]}
-        board[${move:2:1}${move:3:1}]="."
-    fi
-}
-
-# Function to handle user input
-handle_input() {
-    case $key in
-        d) move_forward ;;
-        a) move_backward ;;
-        w) init_board; move_index=0 ;;
-        s) init_board; move_index=0; while [ "$move_index" -lt "${#moves_history[@]}" ]; do move_forward; done ;;
-        q) echo "Exiting."; exit 0 ;;
-        *) echo "Invalid key pressed: $key" ;;
-    esac
-}
-
-# Main script
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <pgn-file>"
-    exit 1
-fi
-
-init_board
-load_pgn "$1"
-move_index=0
-
-while true; do
-    print_board "$move_index"
-    echo "Press 'd' to move forward, 'a' to move back, 'w' to go to the start, 's' to go to the end, 'q' to quit:"
-    read -n 1 key
-    handle_input
 done
